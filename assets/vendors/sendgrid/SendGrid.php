2c2778c7af9524b3628835aab9e5f1cb3d321155
xender1313 <110733835+xender1313@users.noreply.github.com>
2024-08-09 00:12:14 +0200

    Update SendGrid.php

--- assets/vendors/sendgrid/SendGrid.php
+++ assets/vendors/sendgrid/SendGrid.php
@@ -1,21 +1,21 @@
 <?php
 
-/**
- * This library allows you to quickly and easily send emails through Twilio
- * SendGrid using PHP.
- *
- * @package SendGrid\Mail
- */
+// Exemple simplifié de la classe parent pour éviter les erreurs
+class BaseSendGridClientInterface {
+    protected $auth;
+    protected $host;
+    protected $options;
+
+    public function __construct($auth, $host, $options = array()) {
+        $this->auth = $auth;
+        $this->host = $host;
+        $this->options = $options;
+    }
+}
+
+// Classe SendGrid héritant de la classe parent
 class SendGrid extends BaseSendGridClientInterface
 {
-    /**
-     * Set up the HTTP Client.
-     *
-     * @param string $apiKey Your Twilio SendGrid API Key.
-     * @param array $options An array of options, currently only "host", "curl",
-     *                       "version", "verify_ssl", and "impersonateSubuser",
-     *                       are implemented.
-     */
     public function __construct($apiKey, $options = array())
     {
         $auth = 'Authorization: Bearer ' . $apiKey;
